fede6e49d26d96b68540d7fa77cac20593c5484d
rajcantcode <mahadeshwarraj23@gmail.com>
2023-12-26 20:38:46 +0530

    Google OAuth authentication implemented

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { Icons } from "./icons";
+import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
+import UserAccountNav from "./UserAccountNav";
+
+const Navbar = async () => {
+  const session = await getAuthSession();
+  return (
+    <div className="fixed inset-x-0 top-0 z-10 py-3 border-b h-fit bg-zinc-100 border-zinc-500">
+      <div className="container flex items-center justify-between h-full mx-auto max-w-7xl">
+        <Link href="/" className="flex items-center gap-2">
+          <Icons.logo className="w-9 h-9" />
+          <p className="hidden text-sm font-medium text-zinc-700 md:block">
+            Breadit
+          </p>
+        </Link>
+
+        {/* Searchbar */}
+        {session ? (
+          <UserAccountNav user={session.user} />
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            {" "}
+            Sign in
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,43 @@
+import { User } from "next-auth";
+import { FC } from "react";
+import { Avatar } from "./ui/avatar/Avatar";
+import Image from "next/image";
+import {
+  AvatarFallback,
+  AvatarImage,
+  AvatarProps,
+} from "@radix-ui/react-avatar";
+import { Icons } from "./icons";
+
+interface UserAvatarProps extends AvatarProps {
+  user: Pick<User, "name" | "image">;
+}
+
+const UserAvatar: FC<UserAvatarProps> = ({ user, ...props }) => {
+  return (
+    <Avatar {...props}>
+      {user.image ? (
+        <div className="relative w-full h-full aspect-square">
+          <Image
+            fill
+            src={user.image}
+            alt="profile picture"
+            referrerPolicy="no-referrer"
+          />
+          {/* <AvatarImage
+            src={user.image}
+            alt="profile picture"
+            referrerPolicy="no-referrer"
+          /> */}
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span className="sr-only">{user?.name}</span>
+          <Icons.user className="w-4 h-4" />
+        </AvatarFallback>
+      )}
+    </Avatar>
+  );
+};
+
+export default UserAvatar;
