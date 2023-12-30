d6fd4e5962f5a9c77889e3f45475d76816318061
rajcantcode <mahadeshwarraj23@gmail.com>
2023-12-30 11:22:53 +0530

    added type declaration for Session

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -10,7 +10,6 @@ declare module "next-auth/jwt" {
 }
 declare module "next-auth" {
   interface Session {
-    id: UserId;
-    username?: string | null;
+    user: User & { id: UserId; username?: string | null };
   }
 }
