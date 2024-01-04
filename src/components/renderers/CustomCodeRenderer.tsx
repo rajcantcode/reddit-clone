2923630b00cd96d3721ee46c4552be001581c978
rajcantcode <mahadeshwarraj23@gmail.com>
2024-01-04 20:54:02 +0530

    Add CustomCodeRenderer and CustomImageRenderer components for EditorOutput

---
+++ src/components/renderers/CustomCodeRenderer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function CustomCodeRenderer({ data }: any) {
+  data;
+
+  return (
+    <pre className="p-4 bg-gray-800 rounded-md">
+      <code className="text-sm text-gray-100">{data.code}</code>
+    </pre>
+  );
+}
---
+++ src/components/renderers/CustomImageRenderer.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+export default function CustomImageRenderer({ data }: any) {
+  const src = data.file.url;
+  return (
+    <div className="relative w-full min-h-[15rem]">
+      <Image alt="image" className="object-contain" fill src={src}></Image>
+    </div>
+  );
+}
