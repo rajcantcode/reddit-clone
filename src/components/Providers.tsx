b5dd3019dc02c50f1dc1148d62c350b0692c09f8
rajcantcode <mahadeshwarraj23@gmail.com>
2024-01-05 10:59:21 +0530

    Refactor Providers component to include SessionProvider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,10 +1,20 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const Providers = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import { FC, ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const queryClient = new QueryClient();
+
+const Providers: FC<LayoutProps> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryClientProvider>
   );
 };
 
