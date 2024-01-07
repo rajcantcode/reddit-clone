372dee5abf7d4f8848e4944534432eaf9052c90d
rajcantcode <mahadeshwarraj23@gmail.com>
2024-01-07 21:04:40 +0530

    fix customfeed component disappearing

--- src/components/CustomFeed.tsx
+++ src/components/CustomFeed.tsx
@@ -2,23 +2,50 @@ import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import PostFeed from "./PostFeed";
+import { notFound } from "next/navigation";
+import Link from "next/link";
 
 const CustomFeed = async () => {
   const session = await getAuthSession();
+
+  // only rendered if session exists, so this will not happen
+  if (!session) return notFound();
+
   const followedCommunities = await db.subscription.findMany({
     where: {
-      userId: session?.user.id,
+      userId: session.user.id,
     },
     include: {
       subreddit: true,
     },
   });
 
+  if (followedCommunities.length === 0) {
+    return (
+      <div className="flex flex-col col-span-2 space-y-6">
+        <h2>
+          You have not subscribed to any communities. Subscribe to{" "}
+          <Link href={"/r/cats"} className="underline">
+            r/cats{" "}
+          </Link>
+          or{" "}
+          <Link href={"/r/clashRoyale"} className="underline">
+            r/clashRoyale{" "}
+          </Link>
+          to view their posts. Or{" "}
+          <Link href={"r/create"} className="underline">
+            create your own community
+          </Link>
+        </h2>
+      </div>
+    );
+  }
+
   const posts = await db.post.findMany({
     where: {
       subreddit: {
         name: {
-          in: followedCommunities.map(({ subreddit }) => subreddit.name),
+          in: followedCommunities.map((sub) => sub.subreddit.name),
         },
       },
     },
@@ -26,8 +53,8 @@ const CustomFeed = async () => {
       createdAt: "desc",
     },
     include: {
-      author: true,
       votes: true,
+      author: true,
       comments: true,
       subreddit: true,
     },
