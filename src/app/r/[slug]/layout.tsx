5bf8c0571c32e0b18be89f37b8da408f99ef3e5e
rajcantcode <mahadeshwarraj23@gmail.com>
2024-01-08 09:17:41 +0530

    Add ToFeedButton in layout

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,4 +1,5 @@
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import ToFeedButton from "@/components/ToFeedButton";
 // import ToFeedButton from "@/components/ToFeedButton";
 import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
@@ -63,7 +64,7 @@ const Layout = async ({
   return (
     <div className="h-full pt-12 mx-auto sm:container max-w-7xl">
       <div>
-        {/* <ToFeedButton /> */}
+        <ToFeedButton />
 
         <div className="grid grid-cols-1 py-6 md:grid-cols-3 gap-y-4 md:gap-x-4">
           <ul className="flex flex-col col-span-2 space-y-6">{children}</ul>
